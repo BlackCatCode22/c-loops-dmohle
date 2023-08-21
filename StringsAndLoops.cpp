b3eabcf07968ea71776247b811893458481732d6
dmohle <10038913+dmohle@users.noreply.github.com>
2023-08-21 12:10:56 -0700

    class work!

---
+++ StringsAndLoops.cpp
@@ -0,0 +1,22 @@
+// StringsAndLoops.cpp 
+// Dennis Mohle
+// Aug 21, 2023
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 10; i++) {
+        cout << "\n Hello, i is " << i;
+        system("pause");
+    }
+
+
+
+    return 0;
+}
+
